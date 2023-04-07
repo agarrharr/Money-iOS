a08debf2a9523e64e12704022b1b14234c8ffc1c
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2023-04-07 17:48:59 -0600

    Initial Commit

---
+++ Money/MoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyApp.swift
+//  Money
+//
+//  Created by Adam Garrett-Harris on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
