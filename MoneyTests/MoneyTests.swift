0f2db1e5d2a5e469919af4f25fed6dfeb2bab2b7
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2023-04-07 18:46:20 -0600

    Write tests for Expression

--- MoneyTests/MoneyTests.swift
+++ MoneyTests/MoneyTests.swift
@@ -1,36 +1,118 @@
-//
-//  MoneyTests.swift
-//  MoneyTests
-//
-//  Created by Adam Garrett-Harris on 4/7/23.
-//
-
 import XCTest
-@testable import Money
+import Money
 
-final class MoneyTests: XCTestCase {
+extension String: Error {}
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+typealias LedgerDouble = Double
+typealias Commodity = String
+
+struct Amount: Equatable {
+    var value: LedgerDouble
+    var commodity: Commodity?
+    
+    init(value: LedgerDouble, commodity: Commodity? = nil) {
+        self.value = value
+        self.commodity = commodity
     }
+}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+extension Amount {
+    func compute(_ operatorFunction: (LedgerDouble, LedgerDouble) -> LedgerDouble, _ other: Amount) throws -> Amount {
+        guard self.commodity == other.commodity else {
+            throw "Commodities don't match"
+        }
+        return Amount(value: operatorFunction(self.value, other.value), commodity: self.commodity)
     }
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+extension Amount: ExpressibleByIntegerLiteral {
+    init(integerLiteral value: LedgerDouble) {
+        self.value = value
+        self.commodity = nil
     }
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+indirect enum Expression {
+    case amount(Amount)
+    case infixOperator(String, Expression, Expression)
+    case identifier(String)
+}
+
+extension Expression {
+    func evaluate(context: [String: Amount]) throws -> Amount {
+        switch self {
+        case let .amount(amount):
+            return amount
+        case let .infixOperator(op, lhs, rhs):
+            let operators: [String: (LedgerDouble, LedgerDouble) -> LedgerDouble] = [
+                "+": (+),
+                "-": (-),
+                "*": (*),
+                "/": (/)
+            ]
+            guard let operatorFunction = operators[op] else {
+                throw "Undefined operator: \(op)"
+            }
+            let left = try lhs.evaluate(context: context)
+            let right = try rhs.evaluate(context: context)
+            return try left.compute(operatorFunction, right)
+        case let .identifier(name):
+            guard let value = context[name] else {
+                throw "Unknown variable: \(name)"
+            }
+            return value
         }
     }
+}
 
+final class MoneyTests: XCTestCase {
+    func testAmount() throws {
+        // $5
+        let expr = Expression.amount(Amount(value: 5, commodity: "$"))
+        XCTAssertEqual(try! expr.evaluate(context: [:]), Amount(value: 5, commodity: "$"))
+    }
+    
+    func testAddition() {
+        // 5 + 2
+        let expr = Expression.infixOperator("+", .amount(5), .amount(2))
+        XCTAssertEqual(try! expr.evaluate(context: [:]), 7)
+    }
+    
+    func testNestedAddition() {
+        // 2 * 3 * 2
+        let expr = Expression.infixOperator("+", Expression.infixOperator("+", .amount(2), .amount(3)), .amount(2))
+        XCTAssertEqual(try! expr.evaluate(context: [:]), 7)
+    }
+    
+    func testSubtraction() {
+        // 5 - 2
+        let expr = Expression.infixOperator("-", .amount(5), .amount(2))
+        XCTAssertEqual(try! expr.evaluate(context: [:]), 3)
+    }
+    
+    func testMultiplication() {
+        // 5 * 2
+        let expr = Expression.infixOperator("*", .amount(5), .amount(2))
+        XCTAssertEqual(try! expr.evaluate(context: [:]), 10)
+    }
+    
+    func testMultiplicationWithVariable() {
+        // 10 * numberOfPeople
+        let expr = Expression.infixOperator("*", .amount(10), .identifier("numberOfPeople"))
+        let context: [String: Amount] = ["numberOfPeople": 5]
+        XCTAssertEqual(try! expr.evaluate(context: context), 50)
+    }
+    
+    func testDivision() {
+        // 5 / 2
+        let expr = Expression.infixOperator("/", .amount(5), .amount(2))
+        XCTAssertEqual(try! expr.evaluate(context: [:]), Amount(value: 2.5))
+    }
+    
+    func testIdentifier() {
+        // numberOfPeople
+        let expr = Expression.identifier("numberOfPeople")
+        let context: [String: Amount] = ["numberOfPeople": 5]
+        XCTAssertEqual(try! expr.evaluate(context: context), 5)
+    }
 }
